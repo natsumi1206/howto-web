60e346871d7aba2d85a0f72c32bc54a3249e4fab
natsumi.k <k.natsumi.boc@gmail.com>
2021-01-02 20:46:09 +0900

    #1 Change  ESLintのルール緩和とVSCodeの拡張機能と競合しないための設定を加えました

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
   <div id="app">
     <img
       alt="Vue logo"
-      src="./assets/logo.png">
+      src="./assets/logo.png"
+    >
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue"
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 };
-
 </script>
 
 <style>
