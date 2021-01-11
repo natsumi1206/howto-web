0097b42867687ced827428d836f1dfc36286f9e9
natsumi.k <k.natsumi.boc@gmail.com>
2021-01-11 17:36:32 +0900

    #5 Change レスポンシブ対応 サイドバーをタブレット、スマホ時に非表示 タブレット、スマホ時に投稿ボタンを右下に配置

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,17 @@
     <Header />
     <v-container
       fill-height
+      style="position: relative"
     >
-      <Sidebar />
+      <Sidebar id="sidebar" />
+      <v-btn
+        id="post-btn-sp"
+        fab
+        x-large
+        color="teal lighten-1"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
     </v-container>
   </v-app>
 </template>
@@ -23,8 +32,32 @@ export default {
 
 
 <style lang="scss">
-.border-bottom {
-  border-bottom: 1px solid #424242;
+
+#sidebar {
+  @include display_sp {
+    display: none !important;
+  }
+  @include display_tab {
+    display: none !important;
+  }
+}
+
+#post-btn-sp {
+  @include display_sp {
+    bottom: 0;
+    right: 0;
+    position: absolute;
+    margin: 0 20px 50px 0;
+  }
+  @include display_tab {
+    bottom: 0;
+    right: 0;
+    position: absolute;
+    margin: 0 20px 50px 0;
+  }
+  @include display_pc {
+    display: none;
+  }
 }
 
 </style>
