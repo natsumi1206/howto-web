795966e7462d5d493b2186f8b6dfef8130aceb74
natsumi.k <k.natsumi.boc@gmail.com>
2021-01-11 17:27:53 +0900

    #5 Add レスポンシブ対応： メディアクエリファイルをグローバル登録

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: "@import './src/styles/common/common.scss'"
+      },
+      scss: {
+        prependData: "@import './src/styles/common/common.scss';"
+      }
+    }
+  }
 }
