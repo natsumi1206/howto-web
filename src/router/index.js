75057911c2b1bcf6db07f76616166318dc15f08c
natsumi.k <k.natsumi.boc@gmail.com>
2021-01-26 00:21:33 +0900

    router/index.jsに新規登録用ページのルーティングを追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 import Login from "../views/Login.vue"
+import Register from "../views/Register.vue"
 import Profile from "../views/Profile.vue"
 import store from "../store/index.js"
 
@@ -12,25 +13,29 @@ const router = new VueRouter({
   routes: [
     {
       path: "/",
-      meta: {requiresAuth: true}, //ログイン認証が必要なページ
+      meta: {requiresAuth: true},
     },
     {
       path: "/login",
       component: Login
     },
     {
+      path: "/register",
+      component: Register
+    },
+    {
       path: "/profile",
       component: Profile,
-      meta: {requiresAuth: true}, //ログイン認証が必要なページ
+      meta: {requiresAuth: true},
     },
   ]
 })
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some((record) => record.meta.requiresAuth)) {
-    if (!store.getters.loggedIn) { //ログイン状態でなかったら
+    if (!store.getters.loggedIn) {
       next({
-        path: "/login", //ログインページにリダイレクト
+        path: "/login",
         query: {
           redirect: to.fullPath,
         }
